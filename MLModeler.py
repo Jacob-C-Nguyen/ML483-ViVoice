1b90e347975913e49ee1faad3a9d0e2cc66e3f0f
Jacob Nguyen <JacobNguyen714@csu.fullerton.edu>
2025-11-19 14:08:06 -0800

    Small changes. small changes.

--- MLModeler.py
+++ MLModeler.py
@@ -1,3 +1,11 @@
 #let the user upload a voice clip (min time of 30 seconds) and use that to clone the voice
 
+
+from trainer import Trainer, TrainerArgs
+
 #check to see if the computer has cuda capabilities, or use CPU instead.
+
+
+
+
+print("MLModel.py Works!")
